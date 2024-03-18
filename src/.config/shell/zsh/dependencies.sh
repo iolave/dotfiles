dbc9eda7e044eb35f04a852b01471303e237e6a4
Ignacio Olave <olave.ia@icloud.com>
2024-03-17 23:47:19 -0300

    adds installed packages from mbp and mm

--- src/.config/shell/zsh/dependencies.sh
+++ src/.config/shell/zsh/dependencies.sh
@@ -24,8 +24,10 @@ function prompt_yn() {
         fi
 }
 
+UNINSTALLED_DEPENDENCIES=()
 function brew_dependency_check () {
 	DEP=$1
+	PKG=$2
 
 	if [[ "${DEP}" == "" ]]; then
 		echo "[ERROR] brew_dependency_check() called without a dependency name at ${SCRIPT_FILE}"
@@ -33,19 +35,42 @@ function brew_dependency_check () {
 		return 1
 	fi
 
+	if [[ "${PKG}" != "" ]]; then
+		which ${DEP} &> /dev/null
+		IS_INSTALLED=$?
+
+		if [[ "$IS_INSTALLED" != "0" ]]; then
+			UNINSTALLED_DEPENDENCIES+=("${PKG}")
+		fi
+
+		return
+	fi
+
 	which ${DEP} &> /dev/null
 	IS_INSTALLED=$?
 
 	if [[ "$IS_INSTALLED" != "0" ]]; then
-		prompt_yn  "'${DEP}' is not installed"
-        	PROMPT_VALUE=$?
-        	if [[ "$PROMPT_VALUE" == "1" ]]; then
-               		echo "[WARN] ${DEP} not installed"
-			return
-        	fi
-		
-		brew install ${DEP}
+		UNINSTALLED_DEPENDENCIES+=("${DEP}")
 	fi
+}
+
+function install_dependencies() {
+	DEPS=$@
+
+	if [[ "${DEPS}" == "" ]]; then
+		return
+	fi
+
+	prompt_yn "Dependencies '${DEPS}' are not installed"
+        PROMPT_VALUE=$?
+        if [[ "$PROMPT_VALUE" == "1" ]]; then
+            	echo "[WARN] dependencies '${DEPS}' were not installed"
+		return
+	fi
+
+	IFS=" "
+	eval "brew install ${DEPS[*]}"
+
 }
 
 SYSTEM_NAME=$(uname)
@@ -53,7 +78,27 @@ SYSTEM_NAME=$(uname)
 if [[ "${SYSTEM_NAME}" == "Darwin" ]]; then
 	brew_dependency_check jq
 	brew_dependency_check tmux
+	brew_dependency_check curl
+	brew_dependency_check docker
+	brew_dependency_check aws awscli
+	brew_dependency_check htop
+	brew_dependency_check nvim neovim
+	brew_dependency_check nmap 
+	brew_dependency_check openvpn
+	brew_dependency_check telnet
+	brew_dependency_check stow
+	brew_dependency_check volta
+	brew_dependency_check go golang
+	brew_dependency_check mongosh
+	brew_dependency_check rustc rust
+	# brew_dependency_check visual-studio-code
+	# brew_dependency_check tiles
+	# brew_dependency_check alt-tab
 
-	unset -f brew_dependency_check 
+	install_dependencies $UNINSTALLED_DEPENDENCIES
 fi
+
+unset -f brew_dependency_check 
+unset -f install_dependencies
+unset -f prompt_yn
 
