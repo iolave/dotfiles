c537c0c2c6f7d842beb950c91b6d5877040ae632
Ignacio Olave <iolave@ignacios-mm.pingohome.com>
2024-03-26 11:52:08 -0300

    adds homebrew installation when its not installed in macos

--- src/.config/shell/zsh/brew.sh
+++ src/.config/shell/zsh/brew.sh
@@ -73,32 +73,46 @@ function install_dependencies() {
 
 }
 
+which brew &> /dev/null
+IS_BREW_INSTALLED=$?
+if [[ "$IS_BREW_INSTALLED" != "0" ]]; then
+	prompt_yn "homebrew is not installed"
+        PROMPT_VALUE=$?
+	if [[ "$PROMPT_VALUE" == "1" ]]; then
+            	echo "[WARN] homebrew was not installed"
+		return
+	fi
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	which brew &> /dev/null
+	IS_BREW_INSTALLED=$?
+fi
+
 SYSTEM_NAME=$(uname)
+if [[ "$IS_BREW_INSTALLED" == "0" ]]; then
+	if [[ "${SYSTEM_NAME}" == "Darwin" ]]; then
+		brew_dependency_check jq
+		brew_dependency_check tmux
+		brew_dependency_check curl
+		brew_dependency_check docker
+		brew_dependency_check aws awscli
+		brew_dependency_check htop
+		brew_dependency_check nvim neovim
+		brew_dependency_check nmap 
+		brew_dependency_check openvpn
+		brew_dependency_check telnet
+		brew_dependency_check stow
+		brew_dependency_check volta
+		brew_dependency_check go golang
+		brew_dependency_check mongosh
+		brew_dependency_check rustc rust
+		# brew_dependency_check visual-studio-code
+		# brew_dependency_check tiles
+		# brew_dependency_check alt-tab
 
-if [[ "${SYSTEM_NAME}" == "Darwin" ]]; then
-	brew_dependency_check jq
-	brew_dependency_check tmux
-	brew_dependency_check curl
-	brew_dependency_check docker
-	brew_dependency_check aws awscli
-	brew_dependency_check htop
-	brew_dependency_check nvim neovim
-	brew_dependency_check nmap 
-	brew_dependency_check openvpn
-	brew_dependency_check telnet
-	brew_dependency_check stow
-	brew_dependency_check volta
-	brew_dependency_check go golang
-	brew_dependency_check mongosh
-	brew_dependency_check rustc rust
-	# brew_dependency_check visual-studio-code
-	# brew_dependency_check tiles
-	# brew_dependency_check alt-tab
-
-	install_dependencies $UNINSTALLED_DEPENDENCIES
+		install_dependencies $UNINSTALLED_DEPENDENCIES
+	fi
 fi
 
 unset -f brew_dependency_check 
 unset -f install_dependencies
 unset -f prompt_yn
-
