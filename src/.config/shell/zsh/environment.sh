6f3471f1de1e2d529b230fb8a6b2989e1b8f7e4f
Ignacio Olave <me@iolave.com>
2024-03-26 12:29:17 -0300

    adds docker, volta, bun envs

--- src/.config/shell/zsh/environment.sh
+++ src/.config/shell/zsh/environment.sh
@@ -1,4 +1,17 @@
 #!/bin/zsh
 
+# Docker env
 export DOCKER_HOST=ssh://docker@docker.pingolabs.com
+
+# Bun env
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+if [[ "$(uname)" == "Darwin" ]]; then
+        [ -s "/Users/$(whoami)/.bun/_bun" ] && source "/Users/$(whoami)/.bun/_bun"
+fi
+
+# Volta env
+export VOLTA_HOME="$HOME/.volta"
+export PATH="$VOLTA_HOME/bin:$PATH"
+
 export EDITOR=vim
