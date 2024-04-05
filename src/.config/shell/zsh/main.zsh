c30987a2292696195c73030174b0b058dec7a164
Ignacio Olave <me@iolave.com>
2024-04-05 10:09:07 -0300

    renamed zsh config files .sh extension to .zsh

---
+++ src/.config/shell/zsh/main.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# Sets current script root dir
+SCRIPT_FILE=$(readlink -f "$0")
+ROOT_DIR=$(dirname "$SCRIPT_FILE")
+source $ROOT_DIR/oh-my-zsh.zsh
+source $ROOT_DIR/brew.zsh
+source $ROOT_DIR/environment.zsh
+source $ROOT_DIR/aliases.zsh
+source $ROOT_DIR/functions.zsh
+
