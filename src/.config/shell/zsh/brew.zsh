b410b76d7e3d5dda8f265b6e1b37cc82a4175f0f
Ignacio Olave <me@iolave.com>
2024-04-15 13:08:14 -0400

    adds microsoft rdp client

--- src/.config/shell/zsh/brew.zsh
+++ src/.config/shell/zsh/brew.zsh
@@ -142,6 +142,7 @@ if [[ "$IS_BREW_INSTALLED" == "0" ]]; then
 		brew_dependency_check --cask warp
 		brew_dependency_check --cask hiddenbar 
 		brew_dependency_check --cask firefox 
+		brew_dependency_check --cask microsoft-remote-desktop 
 
 		install_dependencies $UNINSTALLED_DEPENDENCIES
 		install_cask_dependencies $UNINSTALLED_CASK_DEPENDENCIES
