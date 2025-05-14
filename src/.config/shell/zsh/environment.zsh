a4353f0554babd3cafe202e6a2288bcd2276e921
Ignacio Olave <me@iolave.com>
2025-05-14 09:27:37 -0400

    commented unused config

--- src/.config/shell/zsh/environment.zsh
+++ src/.config/shell/zsh/environment.zsh
@@ -1,18 +1,18 @@
 #!/bin/zsh
 
 # Docker env
-export DOCKER_HOST=ssh://iolave@10.0.20.20
+# export DOCKER_HOST=ssh://iolave@10.0.20.20
 
 # Bun env
-export BUN_INSTALL="$HOME/.bun"
-PATH="$BUN_INSTALL/bin:$PATH"
-export PATH="${PATH}:${HOME}/go/bin"
-if [[ "$(uname)" == "Darwin" ]]; then
-        [ -s "/Users/$(whoami)/.bun/_bun" ] && source "/Users/$(whoami)/.bun/_bun"
-fi
+# export BUN_INSTALL="$HOME/.bun"
+# PATH="$BUN_INSTALL/bin:$PATH"
+# export PATH="${PATH}:${HOME}/go/bin"
+# if [[ "$(uname)" == "Darwin" ]]; then
+#         [ -s "/Users/$(whoami)/.bun/_bun" ] && source "/Users/$(whoami)/.bun/_bun"
+# fi
 
 # Volta env
-export VOLTA_HOME="$HOME/.volta"
-export PATH="$VOLTA_HOME/bin:$PATH"
+# export VOLTA_HOME="$HOME/.volta"
+# export PATH="$VOLTA_HOME/bin:$PATH"
 
 export EDITOR=vim
